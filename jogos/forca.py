1ec34699fc7438be810aeaa634149fafb935ad1a
vogeljr <roberto_vogel@hotmail.com>
2019-06-12 11:19:11 -0300

    up

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,6 +5,21 @@ def jogar():
 	print("****************************")
 
 
+	palavra_secreta = "banana"
+	enforcou = False
+	acertou = False
+
+	while(not enforcou and not acertou):
+		
+		chute = input("Qual letra? ")
+		chute = chute.strip()
+
+		index = 0
+		for letra in palavra_secreta:
+			if(chute.upper() == letra.upper()):
+				print("Encontrei a letra {} na posição {}" .format(letra, index))
+			index = index + 1	
+
 	print("Fim do jogo")
 
 if(__name__ == "__main__"):
