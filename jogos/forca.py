fd8248a2cc48bd851f8bf1cee538f18cc53b23ac
vogeljr <roberto_vogel@hotmail.com>
2019-06-13 17:20:21 -0300

    up

--- jogos/forca.py
+++ jogos/forca.py
@@ -6,11 +6,8 @@ def jogar():
 
 
 	palavra_secreta  = "banana".upper()
-	letras_acertadas = ["_","_","_","_","_","_",]
+	letras_acertadas = ["_" for letra in letras_acertadas]
 	
-	for letra in palavra_secreta:
-		letras_acertadas.append("_")
-
 
 	enforcou 		 = False
 	acertou 		 = False
